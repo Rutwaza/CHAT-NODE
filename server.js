786a965c1bcb358ad35adecec5eaea2676a22a87
Rutwaza <rutwazanelson1@gmail.com>
2024-10-07 10:13:00 +0200

    updated links

--- server.js
+++ server.js
@@ -378,11 +378,11 @@ app.post('/login', (req, res) => {
 
     connection.query('SELECT * FROM users WHERE Email = ?', [email], (err, results) => {
         if (err) {
-            return res.status(500).sendFile(path.join(__dirname, '/public/error_db.html'));
+            return res.status(500).sendFile(path.join(__dirname, 'https://hackersden-lvr341qj2-nelsons-projects-35a52f9b.vercel.app/error_db.html'));
         }
 
         if (results.length === 0) {
-            return res.status(401).sendFile(path.join(__dirname, '/public/error.html'));
+            return res.status(401).sendFile(path.join(__dirname, 'https://hackersden-lvr341qj2-nelsons-projects-35a52f9b.vercel.app/error.html'));
         }
 
         const user = results[0];
@@ -392,14 +392,14 @@ app.post('/login', (req, res) => {
             req.session.save((err) => { // Ensure the session is saved before redirecting
                 if (err) {
                     console.error('Session save error:', err);
-                    return res.status(500).sendFile(path.join(__dirname, '/public/error_db.html'));
+                    return res.status(500).sendFile(path.join(__dirname, 'https://hackersden-lvr341qj2-nelsons-projects-35a52f9b.vercel.app/error_db.html'));
                 }
                 console.log('User ID:', req.session.userID);
                 console.log('Username:', req.session.username);
-                res.redirect('/homepage');
+                res.redirect('https://hackersden-lvr341qj2-nelsons-projects-35a52f9b.vercel.app/homepage.html');
             });
         } else {
-            res.status(401).sendFile(path.join(__dirname, '/public/error.html'));
+            res.status(401).sendFile(path.join(__dirname, 'https://hackersden-lvr341qj2-nelsons-projects-35a52f9b.vercel.app/error.html'));
         }
     });
 });
